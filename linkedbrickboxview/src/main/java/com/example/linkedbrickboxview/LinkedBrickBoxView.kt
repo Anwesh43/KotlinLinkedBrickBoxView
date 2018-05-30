a9129cf143919d91d82d828cb821981903489430
Anwesh43 <anweshthecool0@gmail.com>
2018-05-30 10:00:50 +0530

    adding linkedboxview

--- linkedbrickboxview/src/main/java/com/example/linkedbrickboxview/LinkedBrickBoxView.kt
+++ linkedbrickboxview/src/main/java/com/example/linkedbrickboxview/LinkedBrickBoxView.kt
@@ -3,3 +3,26 @@ package com.example.linkedbrickboxview
 /**
  * Created by anweshmishra on 30/05/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+
+class LinkedBrickBoxView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
